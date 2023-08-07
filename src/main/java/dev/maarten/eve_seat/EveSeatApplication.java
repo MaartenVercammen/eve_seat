985a6c2ff4a854fc87b16bc19c7a08c0a96313d1
MaartenVercammen <vercammen.maarten.2000@gmail.com>
2023-08-07 18:42:47 +0200

    start of project

---
+++ src/main/java/dev/maarten/eve_seat/EveSeatApplication.java
@@ -0,0 +1,13 @@
+package dev.maarten.eve_seat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveSeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveSeatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/maarten/eve_seat/EveSeatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.maarten.eve_seat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveSeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
